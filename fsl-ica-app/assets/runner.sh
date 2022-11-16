2e12842116bee4da61a2fab42166f42b4fce7c0e
wjallen <wallen@tacc.utexas.edu>
2022-11-16 01:59:46 -0600

    moved fsl melodic app to ls6

--- fsl-ica-app/assets/runner.sh
+++ fsl-ica-app/assets/runner.sh
@@ -1,40 +1,42 @@
 # Allow over-ride
 if [ -z "${CONTAINER_IMAGE}" ]
 then
-    version=$(cat ./_util/VERSION)
-    CONTAINER_IMAGE="index.docker.io/library/ubuntu:bionic"
+    version=6.0.5
+    CONTAINER_IMAGE="index.docker.io/wjallen/fsl:${version}"
 fi
 . lib/container_exec.sh
 
+# silence xalt errors
+module unload xalt
+
 
 COMMAND="melodic"
-
 INPUTS=" -i ${4dimage} "
-
-
 OPTS=""
+
 
 if [ -n "${mask_file}" ];
 then
   OPTS="${OPTS} -m masks/${mask_file} "
-
 else
   echo "must specify mask file"
   exit 1
-
 fi
 
 
 if [ -n "${dim_red}" ];
 then
   OPTS="${OPTS} -d ${dim_red} "
-
 else
   OPTS="${OPTS} -d 20 "
-
 fi
+
 
 OPTS="${OPTS} -a concat --vn --Oall --report "
 
-echo "container_exec ${CONTAINER_IMAGE} ${COMMAND} ${INPUTS} ${OPTS}"
-container_exec ${CONTAINER_IMAGE} $COMMAND $INPUTS $OPTS
+
+echo "================================================================"
+echo "COMMAND = container_exec ${CONTAINER_IMAGE} ${COMMAND} ${INPUTS} ${OPTS}"
+echo "================================================================"
+
+container_exec ${CONTAINER_IMAGE} ${COMMAND} ${INPUTS} ${OPTS}
