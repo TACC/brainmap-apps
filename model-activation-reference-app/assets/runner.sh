6f000bb0db0dc7568dc94142e7b7b80d1e68dfa9
wjallen <wallen@tacc.utexas.edu>
2023-03-26 22:44:07 -0500

    updates to MAR

--- model-activation-reference-app/assets/runner.sh
+++ model-activation-reference-app/assets/runner.sh
@@ -1,98 +1,213 @@
 # Allow over-ride
 if [ -z "${CONTAINER_IMAGE}" ]
 then
-    version=$(cat ./_util/VERSION)
-    CONTAINER_IMAGE="index.docker.io/library/ubuntu:bionic"
+    CONTAINER_IMAGE="wjallen/mar:1.0.0"
 fi
-. lib/container_exec.sh
+
+SING_IMG=$( basename ${CONTAINER_IMAGE} | tr ':' '_' )
+SING_IMG="${SING_IMG}.sif"
+singularity pull --disable-cache ${SING_IMG} docker://${CONTAINER_IMAGE}
+
+# silence xalt errors
 module unload xalt
 
-
-
-SING_IMG='mar_1.0.0.sif'
-TEMPDIR1='./tempdir1'
-TEMPDIR2='./tempdir2'
-TEMPDIR3='./tempdir3'
-OUTPUT='./output'
+# set up temp dirs
+TEMPDIR1="./tempdir1"
+TEMPDIR2="./tempdir2"
+TEMPDIR3="./tempdir3"
+OUTPUT="./output"
 mkdir ${TEMPDIR1}
 mkdir ${TEMPDIR2}
-#mkdir ${TEMPDIR3} # the script makes this one automatically
+mkdir ${TEMPDIR3}
 mkdir ${OUTPUT}
 
 
+# Step 0: Gather input
 
-# Step 0: Gather input (tsv format)
+# input file name
 if [ -n "${input_tsv}" ];
 then
-    INPUT="${input_tsv} "
+    INPUT="${input_tsv}"
 else
-    INPUT="vbm_export_voxels.txt "
+    INPUT="vbm_export_voxels.txt"
 fi
 
-# can be -tal or -mni
-if [ -n "${format}" ];
+# coord space can be -tal or -mni
+if [ -n "${coord_space}" ];
 then
-    FORMAT="${format} "
+    FORMAT="${coord_space}"
 else
-    MASK="-tal "
+    FORMAT="-tal"
 fi
 
-# can be MNI152_wb.nii.gz, MNI152_wb_dil.nii.gz, Tal_wb.nii.gz, Tal_wb_dil.nii.gz
+# mask can be MNI152_wb.nii.gz, MNI152_wb_dil.nii.gz, Tal_wb.nii.gz, Tal_wb_dil.nii.gz
 if [ -n "${mask_file}" ];
 then
-    MASK="masks/${mask_file} "
+    MASK="masks/${mask_file}"
 else
-    MASK="masks/Tal_wb_dil.nii.gz "
+    MASK="masks/Tal_wb_dil.nii.gz"
 fi
+
+# output can be formatted for macm or cbp
+if [ "${output_format}" == "macm" ];
+then
+    OUTPUT_FORMAT="macm"
+    OUTPUT_FLAG="-1d"
+    IMAGE_TYPE="char"
+elif [ "${output_format}" == "cbp" ];
+then
+    OUTPUT_FORMAT="cbp"
+    OUTPUT_FLAG="-cc"
+    IMAGE_TYPE="float"
+else
+    OUTPUT_FORMAT="macm"
+    OUTPUT_FLAG="-1d"
+    IMAGE_TYPE="char"
+fi
+
+
+# Log commands, timing, run job
+echo "================================================================"
+echo "CONTAINER = singularity pull --disable-cache ${SING_IMG} docker://${CONTAINER_IMAGE}"
+echo "================================================================"
+
 
 # Step 1: Split Experiments
 # will generate a bunch of tal text files in the output folder
-CMD1=" python /MAR/splitExperiments.py ${INPUT} ${TEMPDIR1} -nosubj"
-#container_exec ${CONTAINER_IMAGE} ${CMD1}
-singularity pull --disable-cache ${SING_IMG} docker://${CONTAINER_IMAGE}
-singularity --quiet exec ${SING_IMG} ${CMD1}
+CMD1="python /MAR/splitExperiments.py"
+OPT1="${INPUT} ${TEMPDIR1} -nosubj"
+echo "================================================================"
+echo -n "starting step 1: Split experiments, "
+date
+echo "COMMAND1 = singularity exec ${SING_IMG} ${CMD1} ${OPT1}"
+echo "================================================================"
+singularity --quiet exec ${SING_IMG} ${CMD1} ${OPT1}
 
 
-
-# Step 2: Get meta activation map
+# Step 2: Get meta activation map or foci image
 # will generate one nifti image per file
-CP="-cp /app/GingerALE.jar "
-MAIN="org.brainmap.meta.getActivationMap "
-OPT2="-expanded -gzip "
-
+if [ "${output_format}" == "cbp" ];
+then
+    CMD2="java -cp /app/GingerALE.jar org.brainmap.meta.getActivationMap "
+    OPT2="-expanded -gzip $FORMAT -mask=${MASK}"
+elif [ "${output_format}" == "macm" ];
+then
+    CMD2="java -cp /app/GingerALE.jar org.brainmap.meta.getFociImage "
+    OPT2="-gzip $FORMAT -mask=${MASK}"
+fi
+echo "================================================================"
+echo -n "starting step 2: Get meta activation maps or foci images, "
+date
+echo "COMMAND2 = singularity exec ${SING_IMG} ${CMD2} ${OPT2} <each file>"
+echo "================================================================"
+N=40 # N simultaneous processes
 for FILE in ${TEMPDIR1}/*.txt
 do
-    #container_exec ${CONTAINER_IMAGE} java $CP $MAIN $OPT2 $FORMAT -mask=${MASK} $FILE
-    singularity --quiet exec ${SING_IMG} java $CP $MAIN $OPT2 $FORMAT -mask=${MASK} $FILE
-    NEW_FILE=$( basename $FILE .txt )
-    mv ${TEMPDIR1}/${NEW_FILE}_ALE.nii.gz ${TEMPDIR2}/
+    ((i=i%$N)); ((i++==0)) && wait
+    singularity --quiet exec ${SING_IMG} ${CMD2} ${OPT2} $FILE \
+    && NEW_FILE=$( basename $FILE .txt ) \
+    && mv ${TEMPDIR1}/${NEW_FILE}*nii.gz ${TEMPDIR2}/ &
 done
-
 
 
 # Step 3: Scale Images
 # will generate one scaled nifti image per file
-CMD3="bash /MAR/scaleImageDir.sh "
-OPT3="${TEMPDIR2} ${TEMPDIR3} -float "
-#container_exec ${CONTAINER_IMAGE} ${CMD3} ${OPT3}
-singularity --quiet exec ${SING_IMG} ${CMD3} ${OPT3}
+CMD31="fslstats" 
+OPT31="-P 100"
+echo "================================================================"
+echo -n "starting step 3.1: Find scaling factor, "
+date
+echo "COMMAND3.1 = singularity exec ${SING_IMG} ${CMD31} <each file> ${OPT31}"
+echo "================================================================"
+if [ "${output_format}" == "cbp" ];
+then
+    SCALED_MAX="100000"
+    MAX_VAL=0
+    N=40 # N simultaneous processes
+    for FILE in ${TEMPDIR2}/*.nii.gz
+    do
+        ((i=i%$N)); ((i++==0)) && wait
+        THIS_VAL=` singularity --quiet exec ${SING_IMG} ${CMD31} ${FILE} ${OPT31} ` \
+        && echo ${THIS_VAL} >> list_of_vals.txt &
+    done
+    for VALUE in ` cat list_of_vals.txt `
+    do
+        if (( $(echo "$VALUE > $MAX_VAL" | bc -l) )); then
+            MAX_VAL=$VALUE
+        fi
+    done
+    SCALING=` echo "0 k $SCALED_MAX $MAX_VAL / p" | dc - `
+    echo "scaleImageDir $TEMPDIR2 max $MAX_VAL * $SCALING ~= $SCALED_MAX $IMAGE_TYPE"
+    rm list_of_vals.txt
+elif [ "${output_format}" == "macm" ];
+then
+    echo "not scaling because this is macm"
+fi
 
+
+CMD32="fslmaths" 
+OPT32a=""
+if [ "${output_format}" == "cbp" ];
+then
+    OPT32a="-mul $SCALING"
+fi
+OPT32b="-odt $IMAGE_TYPE"
+echo "================================================================"
+echo -n "starting step 3.2: Scale images, "
+date
+echo "COMMAND3.2 = singularity exec ${SING_IMG} ${CMD32} <each file> ${OPT32a} <each out> ${OPT32b}"
+echo "================================================================"
+N=40 # N simultaneous processes
+for FILE in ${TEMPDIR2}/*.nii.gz
+do
+    ((i=i%$N)); ((i++==0)) && wait
+    FILE_BN=$( basename $FILE ) \
+    && singularity --quiet exec ${SING_IMG} ${CMD32} $FILE ${OPT32a} ${TEMPDIR3}/$FILE_BN ${OPT32b} &
+done
 
 
 # Step 4: Merge
 # merge all images into one final image
-CMD4="fslmerge "
+CMD4="fslmerge"
+OPT4="-t ${OUTPUT}/mar_4d.nii.gz ${TEMPDIR3}/*.nii.gz"
+echo "================================================================"
+echo -n "starting step 4: Merge, "
+date
+echo "COMMAND4 = singularity exec ${SING_IMG} ${CMD4} ${OPT4}"
+echo "================================================================"
 export LC_ALL=C
-for IMAGE in ${TEMPDIR3}/*nii.gz
+for FILE in ${TEMPDIR3}/*.nii.gz
 do
-    echo $(basename $IMAGE) >> ${OUTPUT}/mar_4d.txt;
+    echo $(basename $FILE) >> ${OUTPUT}/mar_4d.txt;
 done
-
-#container_exec ${CONTAINER_IMAGE} ${CMD4} -t ${OUTPUT}/mar_4d.nii.gz ${TEMPDIR3}/*nii.gz
-singularity --quiet exec ${SING_IMG} ${CMD4} -t ${OUTPUT}/mar_4d.nii.gz ${TEMPDIR3}/*nii.gz
+singularity --quiet exec ${SING_IMG} ${CMD4} ${OPT4}
 
 
+# Step 5: Generate Voxelwise Output
+# Will generate a ton of output nifti
+# for CBP - currently working but needs to be sped up and files need to be put in more directories
+cd ${OUTPUT}
+CMD5="python3 /MAR/voxelwise.py"
+OPT5="mar_4d.nii.gz ${OUTPUT_FLAG}"
+echo "================================================================"
+echo -n "starting step 5: Generate voxelwise output for ${OUTPUT_FORMAT}, "
+date
+echo "COMMAND5 = singularity exec ${SING_IMG} ${CMD5} ${OPT5}"
+echo "================================================================"
+singularity --quiet exec ../${SING_IMG} ${CMD5} ${OPT5} 
+cd ../ 
 
 
 # Clean up
-rm -rf ${TEMPDIR1} ${TEMPDIR2} ${TEMPDIR3}
+echo "================================================================"
+echo -n "Ending: Packing up output for archive, "
+date
+echo "================================================================"
+mv ${TEMPDIR1} exps_for_macm
+tar -czf exps_for_macm.tar.gz exps_for_macm/         # MACM needs this
+tar -czf output.tar.gz ${OUTPUT}
+find ${OUTPUT} | sort > output_manifest.txt
+rm -rf exps_for_macm/ ${TEMPDIR2} ${TEMPDIR3} ${OUTPUT}
+
+echo -n "Done: "
+date
