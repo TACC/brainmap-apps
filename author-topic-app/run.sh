6c8c847158de75260c8b9f61e1fa9194bc39b911
wjallen <wallen@tacc.utexas.edu>
2024-01-05 10:39:03 -0600

    updated author topic app to tapis v3

--- author-topic-app/run.sh
+++ author-topic-app/run.sh
@@ -1,17 +1,8 @@
-# Allow over-ride
-if [ -z "${CONTAINER_IMAGE}" ]
-then
-    CONTAINER_IMAGE="wjallen/author-topic:1.0.0"
-fi
+#!/bin/bash
 
-SING_IMG=$( basename ${CONTAINER_IMAGE} | tr ':' '_' )
-SING_IMG="${SING_IMG}.sif"
-singularity pull --disable-cache ${SING_IMG} docker://${CONTAINER_IMAGE}
+foci_text=$(find * -type f | grep -v 'tapisjob')
 
-# Silence xalt errors
-module unload xalt
-
-# Grab foci text from user, must be MNI
+# Grab foci text from user, must be special MNI format with annotations
 if [ -n "${foci_text}" ];
 then
 	ENVIRON=" --env foci_text=${foci_text} "
@@ -20,29 +11,21 @@ else
 	exit
 fi
 
-# Needed for LAPACK and BLAS
-BINDPATH=" --bind /opt/intel:/opt/intel "
-
 COMMAND="  /scratch/tacc/apps/matlab/2022b/bin/matlab "
 PARAMS=" -nodesktop -nodisplay -nosplash "
-PARAMS=" ${PARAMS} < matlab_wrapper.m "
-
+PARAMS=" ${PARAMS} < /app/src/matlab_wrapper.m "
 
 # Log commands, timing, run job
 echo -n "starting: "
 date
 
 echo "================================================================"
-echo "CONTAINER = singularity pull --disable-cache ${SING_IMG} docker://${CONTAINER_IMAGE}"
+echo "COMMAND = foci_text=${foci_text} ${COMMAND} ${PARAMS}"
 echo "================================================================"
-echo "COMMAND = singularity exec ${ENVIRON} ${BINDPATH} ${SING_IMG} ${COMMAND} ${PARAMS}"
-echo "================================================================"
-
-singularity exec ${ENVIRON} ${BINDPATH} ${SING_IMG} ${COMMAND} ${PARAMS}
+foci_text=${foci_text} ${COMMAND} ${PARAMS}
 
 echo "================================================================"
 echo "Removing the following intermediate files:"
-echo "  ./author-topic_1.0.0.sif"
 echo "  ./data/ActivationVolumes/*.nii"
 echo "  ./data/BinarySmoothedVolumes/*.nii"
 echo "  ./outputs/K1/alpha100_eta0.01/*.mat"
@@ -51,7 +34,6 @@ echo "  ./outputs/K3/alpha100_eta0.01/*.mat"
 echo "  ./outputs/K4/alpha100_eta0.01/*.mat"
 echo "================================================================"
 
-rm -f ./author-topic_1.0.0.sif
 rm -f ./data/ActivationVolumes/*.nii
 rm -f ./data/BinarySmoothedVolumes/*.nii
 rm -f ./outputs/K1/alpha100_eta0.01/*.mat
